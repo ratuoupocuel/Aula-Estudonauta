6a13623dd24e7a7bd5ca4beeff7eea7d1956dab2
ratuoupocuel <139581215+ratuoupocuel@users.noreply.github.com>
2026-05-26 22:38:45 -0300

    Update aula-de-python.py

--- aula-de-python.py
+++ aula-de-python.py
@@ -1,5 +1,15 @@
-print('ola, mundo!')
-print('7''+''4')
+print('ola, mundo!') # aqui colocar coisas dentro de acentos '' é uma declaravel, uma palavra, então no caso, numeros e acentos se tornam palavras dentro deles '5' '+'
+print('7''+''4') 
 print('ola', '4')
-print('ola' + 4)
-print(ola mundo)
+print('ola' + 'bacaba') # o + só funciona pra palavras não calculos/numeros
+
+nome='gustavo'
+idade=25
+peso=75
+print(nome,idade,peso)
+
+nome=input('qual o seu nome? ')
+idade=input('qual sua idade? ')
+peso=input('qual o seu peso? ')
+print('esse é o seu nome: '+nome,' esse é sua idade: '+idade,' esse é o peso: '+peso+' esse é o resultado!! ')  #brinquei aqui e funcionou!
+
